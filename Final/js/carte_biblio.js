886b8a1f899670411870fbb29f1336fedbb4e642
Sixtinegf <153844709+Sixtinegf@users.noreply.github.com>
2026-06-06 14:34:50 +0200

    Update and rename carte_gif_evol.js to carte_biblio.js

--- Final/js/carte_biblio.js
+++ Final/js/carte_biblio.js
@@ -1,3 +1,11 @@
+const supabaseUrl = "https://tawfytfbhtmdoscdgpbu.supabase.co";
+const supabaseKey = "sb_publishable_BlsdkUhS_7FKGN5uggt2uw_fccuiFDj";
+
+const db = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
 let data, data1; 
 
 let img, icon, iconRed; 
@@ -40,22 +48,117 @@ let minTime = Infinity;
 let maxTime = -Infinity;
 let isDraggingTimeline = false;
 
-let gif15;
-let overlay15Evol;
+let gifFrames15 = [];
+let currentGifFrame15 = 0;
 let gifFinished15 = false;
+let overlay15Evol;
 let gifOverlays = {
+
+  7: {
+    scale: 0.54,
+    rot: 0,
+    ox: 0,
+    oy: 0,
+    opacity: 255
+  },
+
+  12: {
+  scale: 0.8,
+  rot: -1,
+  ox: -450,
+  oy: -100,
+  opacity: 255
+},
+
+14: {
+  scale: 0.8,
+  rot: -1,
+  ox: 70,
+  oy: -20,
+  opacity: 255
+},
   15: {
-    scale: 0.8,
+    scale: 1.6,
     rot: -1,
     ox: 60,
     oy: 25,
     opacity: 255
+  },
+
+   16: {
+    scale: 1.2,
+    rot: -1,
+    ox: 350,
+    oy: 0,
+    opacity: 255
   }
+
+};
+let evol15Offset = { ox: 50, oy: -100 };
+
+
+let evol7Offset = {
+  ox: 0,
+  oy: -50,
+  scale: 2
 };
 
-let gif14;
-let overlay14Evol;
+let evol14Offset = {
+  ox: -15,
+  oy: -150,
+  scale: 1
+};
+
+let evol16Offset = {
+  ox: 0,
+  oy: 0,
+  scale: 2
+};
+// =========================
+// 7e arrondissement
+// =========================
+
+let gifFrames7 = [];
+let currentGifFrame7 = 0;
+let gifFinished7 = false;
+
+let overlay7Evol;
+let overlayHideFrame15 = 200;
+let overlayHideFrame7 = 250;
+
+// =========================
+// 14e arrondissement
+// =========================
+
+let gifFrames14 = [];
+let currentGifFrame14 = 0;
 let gifFinished14 = false;
+
+let overlay14Evol;
+
+// =========================
+// 12e arrondissement
+// =========================
+
+let gifFrames12 = [];
+let currentGifFrame12 = 0;
+let gifFinished12 = false;
+let overlayHideFrame12 = 170;
+
+let overlay12Evol;
+
+let gifFrames16 = [];
+let currentGifFrame16 = 0;
+let gifFinished16 = false;
+let overlayHideFrame16 = 170;
+
+let overlay16Evol;
+
+let evol12Offset = {
+  ox: 0,
+  oy: -50,
+  scale: 2
+};
 
 function preload() {
   data = loadJSON("chantiers-a-paris.json");
@@ -64,8 +167,56 @@ function preload() {
   img = loadImage("test3.png");
   icon = loadImage("Pictogramme.png");
   iconRed = loadImage("Picto2.png");
-  gif15 = loadImage("gif/15eme_evol.gif");
+ 
+for (let i = 0; i < 359; i++) {
+
+  let num = nf(i, 5);
+
+ gifFrames15.push(
+    loadImage("gif15/15eme_" + num + ".png")
+  );
+}
+
+for (let i = 0; i < 293; i++) {
+
+  let num = nf(i, 5);
+
+ gifFrames16.push(
+   loadImage("gif16/16_gif_" + num + ".png")
+ );
+}
+
+for (let i = 0; i < 359; i++) {
+
+  let num = nf(i, 5);
+
+  gifFrames7.push(
+    loadImage("gif7/chantier_7eme_" + num + ".png")
+  );
+}
+
+for (let i = 0; i < 127; i++) {
+
+  let num = nf(i, 5);
+
+  gifFrames12.push(
+    loadImage("gif12/12gif/12gif_" + num + ".png")
+  );
+}
+
+//for (let i = 0; i < 257; i++) {
+
+  //let num = nf(i, 5);
+
+  //gifFrames14.push(
+    //loadImage("gif14/14eme_gif_" + num + ".png")
+ // );
+//}
   overlay15Evol = loadImage("arrondi_evo/15eme_evol.png");
+  overlay16Evol = loadImage("arrondi_evo/16eme_evol.png");
+  overlay7Evol = loadImage("arrondi_evo/7eme_evol.png");
+  overlay12Evol = loadImage("arrondi_evo/12eme_evol.png");
+  overlay14Evol = loadImage("arrondi_evo/14eme_evol.png");
 
   let config = {
     1:{scale:0.5,rot:-1,ox:-30,oy:50},
@@ -110,11 +261,6 @@ function setup() {
   console.log("backBtn:", document.getElementById("backBtn"));
 console.log("ui:", document.getElementById("ui"));
 console.log("intro:", document.getElementById("intro"));
-
-  // =========================
-  // INTRO IMAGE HTML
-  // =========================
- 
 
 
   // =========================
@@ -185,10 +331,7 @@ console.log("intro:", document.getElementById("intro"));
   bind("paris", "Ville de Paris");
   bind("tiers", "Tiers");
   bind("reseaux", "Réseaux");
-
-  // =========================
-  // TIMELINE DOM
-  // =========================
+ 
   // =========================
 // TIMELINE DOM (FIXED)
 // =========================
@@ -306,12 +449,85 @@ if (idArr !== null && overlays[idArr]) {
   let o = overlays[idArr];
 
 // remplacement permanent après animation
-if (idArr === 15 && gifFinished15) {
+let isEvol15 = (idArr === 15 && currentGifFrame15 >= 240);
+let isEvol7 = (
+  idArr === 7 &&
+  currentGifFrame7 >= 200
+);
+
+let isEvol12 = (
+  idArr === 12 &&
+  currentGifFrame12 >= 100
+);
+
+let isEvol14 = (
+  idArr === 14 &&
+  currentGifFrame14 >= 180
+);
+
+let isEvol16 = (
+  idArr === 16 &&
+  currentGifFrame16 >= 180
+);
+
+
+if (isEvol15) {
+
   o = {
     ...o,
-    img: overlay15Evol
+    img: overlay15Evol,
+    ox: o.ox + evol15Offset.ox,
+    oy: o.oy + evol15Offset.oy
   };
+
+} else if (isEvol7) {
+  o = {
+    ...o,
+    img: overlay7Evol,
+    ox: o.ox + evol7Offset.ox,
+    oy: o.oy + evol7Offset.oy,
+    scale: o.scale * evol7Offset.scale
+  };
+
+  
+
+  } else if (isEvol14) {
+
+  o = {
+    ...o,
+    img: overlay14Evol,
+    ox: o.ox + evol14Offset.ox,
+    oy: o.oy + evol14Offset.oy,
+    scale: o.scale * evol14Offset.scale
+  };
+
 }
+
+else if (isEvol12) {
+
+  o = {
+    ...o,
+    img: overlay12Evol,
+    ox: o.ox + evol12Offset.ox,
+    oy: o.oy + evol12Offset.oy,
+    scale: o.scale * evol12Offset.scale
+  };
+
+}
+
+else if (isEvol16) {
+
+  o = {
+    ...o,
+    img: overlay16Evol,
+    ox: o.ox + evol16Offset.ox,
+    oy: o.oy + evol16Offset.oy,
+    scale: o.scale * evol16Offset.scale
+  };
+
+}
+
+
   let arr = data1.find(a => Number(a.c_ar) === idArr);
 
   if (arr) {
@@ -325,10 +541,16 @@ if (idArr === 15 && gifFinished15) {
     let overlayAlpha = 255;
 
     // uniquement pour le 15e
-    if (idArr === 15 && selectedArr === 15) {
+    if (
+  (idArr === 15 && selectedArr === 15) ||
+  (idArr === 7 && selectedArr === 7)
+  || (idArr === 12 && selectedArr === 12)
+  || (idArr === 14 && selectedArr === 14)
+  || (idArr === 16 && selectedArr === 16)
+) {
 
-      let revealZoom = 1.2;
-      let fullZoom = 3;
+      let revealZoom = 1;
+      let fullZoom = 2;
 
       let gifAlpha = map(
         scaleFactor,
@@ -347,25 +569,68 @@ if (idArr === 15 && gifFinished15) {
     // si totalement invisible → skip
     if (overlayAlpha > 1) {
 
-      // masque l'overlay quand le gif est totalement visible
+
+// masque l'overlay quand le gif est totalement visible
+// disparition anticipée des overlays
+
+let hideOverlay = false;
+
+// =========================
+// cacher UNIQUEMENT les anciens overlays
+// =========================
+
+// 15e
 if (
   idArr === 15 &&
   selectedArr === 15 &&
-  scaleFactor >= 3
+  currentGifFrame15 >= overlayHideFrame15 &&
+  currentGifFrame15 < 280 // avant apparition du evol
 ) {
-  return;
+  hideOverlay = true;
 }
-// masque l'overlay quand le gif est totalement visible
-let hideOverlay =
-  idArr === 15 &&
-  selectedArr === 15 &&
-  scaleFactor >= 2.5;
+
+if (
+  idArr === 12 &&
+  selectedArr === 12 &&
+  currentGifFrame12 >= overlayHideFrame12 &&
+  currentGifFrame12 < 160
+) {
+  hideOverlay = true;
+}
+
+// 7e
+if (
+  idArr === 7 &&
+  selectedArr === 7 &&
+  currentGifFrame7 >= overlayHideFrame7 &&
+  currentGifFrame7 < 250 // avant apparition du evol
+) {
+  hideOverlay = true;
+}
+
+if (
+  idArr === 16 &&
+  selectedArr === 16 &&
+  currentGifFrame16 >= overlayHideFrame16 &&
+  currentGifFrame16 < 250 // avant apparition du evol
+) {
+  hideOverlay = true;
+}
+
+
+
+// =========================
+// DRAW OVERLAY
+// =========================
 
 if (!hideOverlay) {
 
   push();
 
-  translate(c.x + o.ox + globalFixX, c.y + o.oy + globalFixY);
+  translate(
+    c.x + o.ox + globalFixX,
+    c.y + o.oy + globalFixY
+  );
 
   rotate(radians(o.rot));
 
@@ -387,24 +652,20 @@ if (!hideOverlay) {
 
 
 // =========================
-// GIF 15e progressif
+// SEQUENCE 15e
 // =========================
 
-if (selectedArr === 15 && gif15) {
+if (selectedArr === 15 && !gifFinished15) {
 
   let arr = data1.find(a => Number(a.c_ar) === 15);
 
-  if (arr) {
+  if (arr && gifFrames15.length > 0) {
 
     let c = getArrCenter(arr);
     let g = gifOverlays[15];
 
-    // =========================
-    // APPARITION PROGRESSIVE
-    // =========================
-
-    let revealZoom = 1.2;
-    let fullZoom = 3;
+    let revealZoom = 1;
+    let fullZoom = 2;
 
     let alpha = map(
       scaleFactor,
@@ -416,28 +677,7 @@ if (selectedArr === 15 && gif15) {
 
     alpha = constrain(alpha, 0, 255);
 
-    // =========================
-    // TIMER GIF
-    // =========================
-
-    let gifDuration = 13000;
-
-    if (!window.gifStart15 && alpha > 1) {
-      window.gifStart15 = millis();
-    }
-
-    if (
-      window.gifStart15 &&
-      millis() - window.gifStart15 >= gifDuration
-    ) {
-      gifFinished15 = true;
-    }
-
-    // =========================
-    // AFFICHAGE GIF
-    // =========================
-
-    if (alpha > 1 && !gifFinished15) {
+    if (alpha > 1) {
 
       push();
 
@@ -452,17 +692,317 @@ if (selectedArr === 15 && gif15) {
 
       tint(255, alpha);
 
+      let frameImg = gifFrames15[currentGifFrame15];
+
       image(
-        gif15,
+        frameImg,
         0,
         0,
-        gif15.width * g.scale,
-        gif15.height * g.scale
+        frameImg.width * g.scale,
+        frameImg.height * g.scale
       );
 
       noTint();
 
       pop();
+
+  // vitesse animation
+if (frameCount % 2 === 0) {
+
+  // avance seulement si pas à la dernière frame
+  if (currentGifFrame15 < gifFrames15.length - 1) {
+
+    currentGifFrame15++;
+
+  } else {
+
+    // animation terminée
+    gifFinished15 = true;
+  }
+}
+    }
+  }
+}
+
+
+// =========================
+// SEQUENCE 16e
+// =========================
+
+if (selectedArr === 16 && !gifFinished16) {
+
+  let arr = data1.find(a => Number(a.c_ar) === 16);
+
+  if (arr && gifFrames16.length > 0) {
+
+    let c = getArrCenter(arr);
+    let g = gifOverlays[16];
+
+    let revealZoom = 1;
+    let fullZoom = 2;
+
+    let alpha = map(
+      scaleFactor,
+      revealZoom,
+      fullZoom,
+      0,
+      255
+    );
+
+    alpha = constrain(alpha, 0, 255);
+
+    if (alpha > 1) {
+
+      push();
+
+      translate(
+        c.x + g.ox + globalFixX,
+        c.y + g.oy + globalFixY
+      );
+
+      rotate(radians(g.rot));
+
+      imageMode(CENTER);
+
+      tint(255, alpha);
+
+      let frameImg = gifFrames16[currentGifFrame16];
+
+      image(
+        frameImg,
+        0,
+        0,
+        frameImg.width * g.scale,
+        frameImg.height * g.scale
+      );
+
+      noTint();
+
+      pop();
+
+  // vitesse animation
+if (frameCount % 1 === 0) {
+
+  // avance seulement si pas à la dernière frame
+  if (currentGifFrame16 < gifFrames16.length - 1) {
+
+    currentGifFrame16++;
+
+  } else {
+
+    // animation terminée
+    gifFinished16 = true;
+  }
+}
+    }
+  }
+}
+
+// =========================
+// SEQUENCE 7e
+// =========================
+
+if (selectedArr === 7 && !gifFinished7) {
+
+  let arr = data1.find(a => Number(a.c_ar) === 7);
+
+  if (arr && gifFrames7.length > 0) {
+
+    let c = getArrCenter(arr);
+    let g = gifOverlays[7];
+
+    let revealZoom = 1;
+    let fullZoom = 2;
+
+    let alpha = map(
+      scaleFactor,
+      revealZoom,
+      fullZoom,
+      0,
+      255
+    );
+
+    alpha = constrain(alpha, 0, 255);
+
+    if (alpha > 1) {
+
+      push();
+
+      translate(
+        c.x + g.ox + globalFixX,
+        c.y + g.oy + globalFixY
+      );
+
+      rotate(radians(g.rot));
+
+      imageMode(CENTER);
+
+      tint(255, alpha);
+
+      let frameImg = gifFrames7[currentGifFrame7];
+
+      image(
+        frameImg,
+        0,
+        0,
+        frameImg.width * g.scale,
+        frameImg.height * g.scale
+      );
+
+      noTint();
+
+      pop();
+
+      // vitesse animation
+      if (frameCount % 1 === 0) {
+
+        if (currentGifFrame7 < gifFrames7.length - 1) {
+
+          currentGifFrame7++;
+
+        } else {
+
+          gifFinished7 = true;
+        }
+      }
+    }
+  }
+}
+
+// =========================
+// SEQUENCE 14e
+// =========================
+
+if (selectedArr === 14 && !gifFinished14) {
+
+  let arr = data1.find(a => Number(a.c_ar) === 14);
+
+  if (arr && gifFrames14.length > 0) {
+
+    let c = getArrCenter(arr);
+    let g = gifOverlays[14];
+
+    let revealZoom = 1;
+    let fullZoom = 2;
+
+    let alpha = map(
+      scaleFactor,
+      revealZoom,
+      fullZoom,
+      0,
+      255
+    );
+
+    alpha = constrain(alpha, 0, 255);
+
+    if (alpha > 1) {
+
+      push();
+
+      translate(
+        c.x + g.ox + globalFixX,
+        c.y + g.oy + globalFixY
+      );
+
+      rotate(radians(g.rot));
+
+      imageMode(CENTER);
+
+      tint(255, alpha);
+
+      let frameImg = gifFrames14[currentGifFrame14];
+
+      image(
+        frameImg,
+        0,
+        0,
+        frameImg.width * g.scale,
+        frameImg.height * g.scale
+      );
+
+      noTint();
+
+      pop();
+
+      // vitesse animation
+      if (frameCount % 1 === 0) {
+
+        if (currentGifFrame14 < gifFrames14.length - 1) {
+
+          currentGifFrame14++;
+
+        } else {
+
+          gifFinished14 = true;
+        }
+      }
+    }
+  }
+}
+
+if (selectedArr === 12 && !gifFinished12) {
+
+  let arr = data1.find(a => Number(a.c_ar) === 12);
+
+  if (arr && gifFrames12.length > 0) {
+
+    let c = getArrCenter(arr);
+    let g = gifOverlays[12];
+
+    let revealZoom = 0.80;
+    let fullZoom = 1.60;
+
+    let alpha = map(
+      scaleFactor,
+      revealZoom,
+      fullZoom,
+      0,
+      255
+    );
+
+    alpha = constrain(alpha, 0, 255);
+
+    if (alpha > 1) {
+
+      push();
+
+      translate(
+        c.x + g.ox + globalFixX,
+        c.y + g.oy + globalFixY
+      );
+
+      rotate(radians(g.rot));
+
+      imageMode(CENTER);
+
+      tint(255, alpha);
+
+      let frameImg = gifFrames12[currentGifFrame12];
+
+      image(
+        frameImg,
+        0,
+        0,
+        frameImg.width * g.scale,
+        frameImg.height * g.scale
+      );
+
+      noTint();
+
+      pop();
+
+      // vitesse animation
+      if (frameCount % 1 === 0) {
+
+        if (currentGifFrame12 < gifFrames12.length - 1) {
+
+          currentGifFrame12++;
+
+        } else {
+
+          gifFinished12 = true;
+        }
+      }
     }
   }
 }
@@ -556,11 +1096,139 @@ if (currentTime < start - buffer || currentTime > end + buffer) continue;
     let isActive = filters[category];
     let imgToUse = isActive ? iconRed : icon;
 
-    // =========================
-    // DRAW POINT
-    // =========================
-    imageMode(CENTER);
-    image(imgToUse, x + ox, y + oy, size, size);
+// cacher les pictos pendant les animations
+
+let hidePoints = false;
+
+// =========================
+// 15e
+// =========================
+
+if (selectedArr === 15 && !gifFinished15) {
+
+  let revealZoom = 1;
+  let fullZoom = 2.5;
+
+  let alpha15 = map(
+    scaleFactor,
+    revealZoom,
+    fullZoom,
+    0,
+    255
+  );
+
+  alpha15 = constrain(alpha15, 0, 255);
+
+  // cache seulement quand le gif est totalement visible
+  if (alpha15 >= 120) {
+    hidePoints = true;
+  }
+}
+
+// =========================
+// 16e
+// =========================
+
+if (selectedArr === 16 && !gifFinished16) {
+
+  let revealZoom = 1;
+  let fullZoom = 2.5;
+
+  let alpha16 = map(
+    scaleFactor,
+    revealZoom,
+    fullZoom,
+    0,
+    255
+  );
+
+  alpha16 = constrain(alpha16, 0, 255);
+
+  // cache seulement quand le gif est totalement visible
+  if (alpha16 >= 120) {
+    hidePoints = true;
+  }
+}
+
+// =========================
+// 7e
+// =========================
+
+if (selectedArr === 7 && !gifFinished7) {
+
+  let revealZoom = 1;
+  let fullZoom = 2.5;
+
+  let alpha7 = map(
+    scaleFactor,
+    revealZoom,
+    fullZoom,
+    0,
+    255
+  );
+
+  alpha7 = constrain(alpha7, 0, 255);
+
+  if (alpha7 >= 120) {
+    hidePoints = true;
+  }
+}
+
+if (selectedArr === 14 && !gifFinished14) {
+
+  let revealZoom = 1;
+  let fullZoom = 2.5;
+
+  let alpha14 = map(
+    scaleFactor,
+    revealZoom,
+    fullZoom,
+    0,
+    255
+  );
+
+  alpha14 = constrain(alpha14, 0, 255);
+
+  if (alpha14 >= 120) {
+    hidePoints = true;
+  }
+}
+
+if (selectedArr === 12 && !gifFinished12) {
+
+  let revealZoom = 1;
+  let fullZoom = 2.5;
+
+  let alpha12 = map(
+    scaleFactor,
+    revealZoom,
+    fullZoom,
+    0,
+    255
+  );
+
+  alpha12 = constrain(alpha12, 0, 255);
+
+  if (alpha12 >= 120) {
+    hidePoints = true;
+  }
+}
+   // =========================
+// DRAW POINT
+// =========================
+
+if (!hidePoints) {
+
+  imageMode(CENTER);
+
+  image(
+    imgToUse,
+    x + ox,
+    y + oy,
+    size,
+    size
+  );
+}
   }
 
   pop();
@@ -605,6 +1273,18 @@ function mouseReleased() {
 }
 
 function mouseWheel(event) {
+
+  const panel = document.getElementById("photoPanel");
+
+  if (
+    panel &&
+    panel.style.display !== "none" &&
+    panel.matches(":hover")
+  ) {
+    return;
+  }
+
+
   let old = scaleFactor;
 
   scaleFactor *= (event.delta < 0 ? 1.05 : 0.95);
@@ -628,14 +1308,24 @@ function mouseWheel(event) {
 }
 
 function doubleClicked() {
+
   const x = (mouseX - offsetX) / scaleFactor;
   const y = (mouseY - offsetY) / scaleFactor;
 
   for (let arr of data1) {
+
     if (pointInArrondissement(x, y, arr)) {
+
       selectedArr = Number(arr.c_ar);
-      zoomMode = "focus"; // 🔥 on débloque le zoom
+
+      zoomMode = "focus";
+
       focus(arr);
+
+      loadArrondissementPhotos(
+        selectedArr
+      );
+
       return;
     }
   }
@@ -650,17 +1340,23 @@ function resetView() {
   offsetY = (height - img.height * scaleFactor) / 2;
 }
 function resetMap() {
+
   selectedArr = null;
+
   zoomMode = "global";
 
   resetView();
 
   offsetX = (width - img.width * scaleFactor) / 2;
   offsetY = (height - img.height * scaleFactor) / 2;
+
+  document
+    .getElementById("photoPanel")
+    .style.display = "none";
 }
 function focus(arr) {
   let c = getArrCenter(arr);
-  scaleFactor = 1;
+  scaleFactor = 0.7;
   offsetX = width / 2 - c.x * scaleFactor;
   offsetY = height / 2 - c.y * scaleFactor;
 }
@@ -775,4 +1471,43 @@ function mapTimeNonLinear(t) {
 
   let localT = (t - beforeWeight - focusWeight) / afterWeight;
   return lerp(focusEnd, maxTime, localT);
+}
+
+async function loadArrondissementPhotos(arrId){
+
+  const { data, error } = await db
+    .from("images")
+    .select("*")
+    .eq("arrondissement", arrId)
+    .order("created_at", {
+      ascending:false
+    });
+
+  if(error){
+
+    console.log(error);
+
+    return;
+  }
+
+  const gallery =
+    document.getElementById("photoGallery");
+
+  gallery.innerHTML = "";
+
+  data.forEach(photo => {
+
+    gallery.innerHTML += `
+      <img src="${photo.url}">
+    `;
+
+  });
+
+  document
+    .getElementById("photoPanel")
+    .style.display = "block";
+
+    const photoPanel =
+  document.getElementById("photoPanel");
+
 }
